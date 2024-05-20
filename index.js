d98e075e628658f062c65fbda38a4077ec20b6be
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-05-20 10:59:33 +0530

    backend code

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
 
-let  createRandomUser = () =>  {
+let  gateRandomUser = () =>  {
     return {
-      userId: faker.string.uuid(),
+      Id: faker.string.uuid(),
       username: faker.internet.userName(),
       email: faker.internet.email(),
-      avatar: faker.image.avatar(),
       password: faker.internet.password(),
-      birthdate: faker.date.birthdate(),
-      registeredAt: faker.date.past(),
     };
   }
+
+  console.log(gateRandomUser());
   
   
