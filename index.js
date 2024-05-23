baa0810765ac5c8eae4d663cdf01491e289cc571
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-05-23 22:13:11 +0530

    database

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ const conn = mysql.createConnection({
     password: 'binay12#'
   });
   
+  let  gateRandomUser = () =>  {
+    return [
+      faker.string.uuid(),
+      faker.internet.userName(),
+      faker.internet.email(),
+      faker.internet.password(),
+    ];
+  }
+
+  let q = "INSERT INTO  user (id, username, email, password) VALUES ?";
+    
+    let data = [];
+    for(let i=0; i<=100; i++) {
+        data.push(gateRandomUser());
+    }
+
   try {
     conn.query(q, [users],  (err, result) => {
     if(err) throw err;
@@ -19,22 +35,10 @@ const conn = mysql.createConnection({
 
 conn.end();
 
-    let q = "INSERT INTO  user (id, username, email, password) VALUES ?";
     
-    let data = [];
-    for(let i=0; i<=100; i++) {
-        data.push(gateRandomUser());
-    }
 
 
-let  gateRandomUser = () =>  {
-    return [
-      faker.string.uuid(),
-      faker.internet.userName(),
-      faker.internet.email(),
-      faker.internet.password(),
-    ];
-  }
+
 
 
   
