90b360ffc95b488d6ba9fedff058553e4bec8000
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-05-26 11:38:01 +0530

    backend code

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const mysql = require('mysql2');
 const express = require("express");
 const app = express();
 const path = require("path");
+const methodOverride = require("method-override");
 
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
