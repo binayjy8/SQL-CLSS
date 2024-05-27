8693c389c27729fe310bbfe674c4e96d0b546ebe
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-05-27 23:33:37 +0530

    backend code

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ const conn = mysql.createConnection({
       }
   });
 
-  app.patch("")
+  app.patch("/user", (req, res))
 
   app.listen("8080", ()=> {
     console.log("listening to the port 8080");
